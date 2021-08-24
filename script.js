0ce734b9bc3823ef0455ad48092d3d1af0e38682
Jozeanne Belomo <jozeanne.belomo@gmail.com>
2021-08-24 00:40:31 -0300

    Adiciona lista de 10 moedas da resposta da API

--- script.js
+++ script.js
@@ -10,7 +10,52 @@ fetch(
         "Erro ao executar a requisição, status " + response.status
       );
 
-    return response.json;
+    return response.json();
   })
-  .then((api) => console.log(api))
+  .then((api) => {
+    let texto = `
+    <div class="container" style="background-color:green;">
+        <div class="row" style="padding-top: 8px;" >
+          <div class="col-lg-4" >
+              <h5 class="mt-2">Nome</h5>
+          </div>
+          <div class="col-lg-4"">
+              <h5 class="mt-2">Símbolo</h5>
+          </div>
+          <div class="col-lg-4">
+              <h5 class="mt-2">Primeiros Dados Históricos em</h5>
+          </div>
+        </div>
+      </div>  
+    `;
+
+    for (let contador = 0; contador < 10; contador++) {
+      const moeda = api.data[contador];
+
+      let data = new Date(moeda.first_historical_data);
+      let dataFormatada =
+        data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear();
+
+      const backgroundColor = contador % 2 == 0 ? "white" : "lightgray";
+
+      texto =
+        texto +
+        `
+      <div class="container" style="background-color:${backgroundColor};">
+        <div class="row" style="padding-top: 8px;" >
+          <div class="col-lg-4" >
+              <p style="padding-top: 6px;">${moeda.name}</p>
+          </div>
+          <div class="col-lg-4"">
+              <p style="padding-top: 6px;">${moeda.symbol}</p>
+          </div>
+          <div class="col-lg-4">
+              <p style="padding-top: 6px;">${dataFormatada}</ṕ>
+          </div>
+        </div>
+      </div>  
+      `;
+    }
+    document.getElementById("coins").innerHTML = texto;
+  })
   .catch((error) => console.error(error.message));
